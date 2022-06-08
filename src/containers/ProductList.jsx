f903c717f726269d35bdae6582e259a9f370be8c
Carlos Hermosillo <hermcode@gmail.com>
2022-06-08 07:21:17 -0500

    [modified] some code structure

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import ProductItem from '@components/ProductItem';
 import { useGetProducts } from '../hooks/useGetProducts';
+import SpinnerLoader from '../components/SpinnerLoader';
 
 import '@styles/ProductList.scss';
-import SpinnerLoader from '../components/SpinnerLoader';
 
 const API = 'https://api.escuelajs.co/api/v1/products?limit=21&offset=1'
 
 const ProductList = () => {
-	
+
 	const {products, isLoading} = useGetProducts(API)
 
 	return (
